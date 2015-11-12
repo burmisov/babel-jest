5a78e61a74a0d170e20b3ac3adb5702ab8ed5c68
Sergey Burmisov <skypath@gmail.com>
2015-11-12 21:12:18 +0300

    use project-level options from .babelrc or package.json

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var babel = require("babel-core");
+var merge = require("babel-core/lib/helpers/merge");
+var OptionManager = require(
+  "babel-core/lib/transformation/file/options/option-manager"
+);
+
+var optsManager = new OptionManager();
+var options = optsManager.init({
+  filename: __filename,
+  babelrc: true
+});
 
 module.exports = {
   process: function (src, filename) {
     // Ignore all files within node_modules
     // babel files can be .js, .es, .jsx or .es6
     if (filename.indexOf("node_modules") === -1 && babel.util.canCompile(filename)) {
-      return babel.transform(src, {
+      return babel.transform(src, merge({
         filename: filename,
         retainLines: true
-      }).code;
+      }, options)).code;
     }
 
     return src;
